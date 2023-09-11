cfea29780eeb0087cf910751b79c5fb5ca86fc3e
Erkan Turker <turker@Erkans-MacBook-Pro.local>
2023-09-10 22:17:24 -0400

    optimizing image size

---
+++ src/services/image-url.ts
@@ -0,0 +1,13 @@
+const getCroppedImageUrl = (url: string) => {
+    // Define the target string to find within the URL.
+    const target = "media/";
+
+    // Find the index of the target string and add its length to get the starting index.
+    const index = url.indexOf(target) + target.length;
+
+    // Slice the URL to extract the part before the "media/" and after it.
+    // Add "/crop/600/400/" in between to create the cropped image URL.
+    return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+}
+
+export default getCroppedImageUrl;
