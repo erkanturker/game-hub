7af0307c445ce0cbce2472ea66eca3f8bea62c1d
Erkan Turker <erkanturker@Erkans-MacBook-Pro.local>
2023-11-29 03:54:05 -0500

    adding slug name to the link

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
 	game: Game;
@@ -21,7 +22,9 @@ const GameCard = ({ game }: Props) => {
 					></PlatformIconList>
 					<CriticScore score={game.metacritic}></CriticScore>
 				</HStack>
+				<Link to={"/games/"+ game.slug}>
 				<Heading fontSize="2xl">{game.name}</Heading>
+				</Link>
 				<Emoji rating={game.rating_top} />
 			</CardBody>
 		</Card>
