cccecccf052d7546ff5be40a5321fbefba63597f
Erkan Turker <erkanturker@Erkans-MacBook-Pro.local>
2023-11-29 07:29:35 -0500

    added expandle text Component

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,24 +1,22 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Heading, Spinner, Text } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailsPage = () => {
+  const { slug } = useParams();
 
-    
-   const {slug} = useParams();
-    
+  const { data: game, isLoading, error } = useGame(slug!);
 
-    const {data:game,isLoading,error} = useGame(slug!);
-
-    if(isLoading) return <Spinner/>
-    if(error||!game) throw error;
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
 
   return (
     <>
-    <Heading>{game.name}</Heading>
-    <Text>{game.description_raw}</Text>
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
-  )
-}
+  );
+};
 
 export default GameDetailsPage;
